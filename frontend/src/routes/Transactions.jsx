a52be158cb625da1ef9ca6dcff6e0a2fc5515654
Kaung Sithu Hlaing <kaungsithuhlaing@gmail.com>
2023-01-14 16:54:55 +0800

    some progess frontend

--- frontend/src/routes/Transactions.jsx
+++ frontend/src/routes/Transactions.jsx
@@ -1,29 +1,43 @@
+import Navbar from "../components/Navbar";
+
 export default function Transactions() {
   // const transactions = JSON.parse("../data/transactions.json");
   // console.log(transactions);
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <ul class="list-disc">
-        <li>
-          Now this is a story all about how, my life got flipped-turned upside
-          down
-        </li>
-      </ul>
-
-      <ol class="list-decimal">
-        <li>
-          Now this is a story all about how, my life got flipped-turned upside
-          down
-        </li>
-      </ol>
-
-      <ul class="list-none">
-        <li>
-          Now this is a story all about how, my life got flipped-turned upside
-          down
-        </li>
-      </ul>
-    </div>
+    <>
+      <Navbar />
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>1</th>
+              <td>Cy Ganderton</td>
+              <td>Quality Control Specialist</td>
+              <td>Blue</td>
+            </tr>
+            <tr className="hover">
+              <th>2</th>
+              <td>Hart Hagerty</td>
+              <td>Desktop Support Technician</td>
+              <td>Purple</td>
+            </tr>
+            <tr>
+              <th>3</th>
+              <td>Brice Swyre</td>
+              <td>Tax Accountant</td>
+              <td>Red</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
