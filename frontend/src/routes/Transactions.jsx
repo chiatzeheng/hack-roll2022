0fe8e451f798a91e5b559710ae40f05c96d7d3bb
Kaung Sithu Hlaing <kaungsithuhlaing@gmail.com>
2023-01-14 16:18:28 +0800

    frontend i guess

---
+++ frontend/src/routes/Transactions.jsx
@@ -0,0 +1,29 @@
+export default function Transactions() {
+  // const transactions = JSON.parse("../data/transactions.json");
+  // console.log(transactions);
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <ul class="list-disc">
+        <li>
+          Now this is a story all about how, my life got flipped-turned upside
+          down
+        </li>
+      </ul>
+
+      <ol class="list-decimal">
+        <li>
+          Now this is a story all about how, my life got flipped-turned upside
+          down
+        </li>
+      </ol>
+
+      <ul class="list-none">
+        <li>
+          Now this is a story all about how, my life got flipped-turned upside
+          down
+        </li>
+      </ul>
+    </div>
+  );
+}
